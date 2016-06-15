294663be596fa62231d30ff18c243ddb041949b7
三木四水 <@qq.com>
2016-06-15 16:26:53 +0800

    项目准备完成

---
+++ day14网易新闻/day14网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day14网易新闻
+//
+//  Created by Jack.Lin on 16/6/15.
+//  Copyright © 2016年 Jack.Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
